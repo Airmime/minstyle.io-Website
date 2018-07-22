47c98b76dbba10f86b7953a804082fc67add81ea
Airmime <r.marion42@hotmail.fr>
2018-07-22 23:54:58 +0200

    Start minstyle.io website project
    Added website structure

---
+++ app.js
@@ -0,0 +1,22 @@
+var compression = require('compression');
+var express = require('express');
+var indexRouter = require('./routes/index');
+var docRouter = require('./routes/doc');
+
+var app = express();
+
+/* GZIP compression */
+app.use(compression());
+
+/* Static ressources */
+app.use(express.static(__dirname + '/public'));
+
+/* View engine */
+app.set('view engine', 'ejs');
+
+/* Routes */
+app.use('/', indexRouter);
+app.use('/doc', docRouter);
+
+/* Server Config */
+app.listen(8080);
