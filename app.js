48bfa1575bcdc8ba84b6030cbc5dc5dfd1eb5410
Airmime <r.marion42@hotmail.fr>
2018-07-24 22:55:33 +0200

    Translation, Session, CSS changes
    - Added Translation with i18n
    - Added middleware session
    - CSS changes
    - Added Started page

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var compression = require('compression');
 var express = require('express');
-var indexRouter = require('./routes/index');
-var docRouter = require('./routes/doc');
+var i18n=require("i18n-express"); 
+var session = require('express-session')
 
 var app = express();
 
@@ -14,9 +14,30 @@ app.use(express.static(__dirname + '/public'));
 /* View engine */
 app.set('view engine', 'ejs');
 
+/* Session */
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+/* Translation */
+app.use(i18n({
+  translationsPath: __dirname + '/i18n', // <--- use here. Specify translations files path.
+  siteLangs: ["en","fr"],
+  textsVarName: 'translation',
+}));
+
 /* Routes */
-app.use('/', indexRouter);
-app.use('/documentation', docRouter);
+app.get('/', function(req, res, next) {
+  res.render('index');
+})
+.get('/doc', function(req, res, next) {
+  res.render('doc');
+})
+.get('/doc/get-started', function(req, res, next) {
+  res.render('doc/get-started');
+});
 
 /* Server Config */
 app.listen(8080);
