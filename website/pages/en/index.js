76c3af64db1b09ef68383d5fccd15c2aa49c0c73
Airmime <r.marion42@hotmail.fr>
2019-09-11 22:06:41 +0200

    Change menu
    - change main menu :hover and :active

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -5,14 +5,20 @@ const CompLibrary = require('../../core/CompLibrary.js');
 class Index extends React.Component {
   render() {
     return (
-    <div class="homecontainer">
-      <div class="welcome"> 
-        <h1>
-          Simple & <span class="light">light</span><br/> open source <span class="css">CSS</span><br/> framework <span class="ms-text-gray">:)</span>
-        </h1>
-        <p>            
-          minstyle.io integrates a set of pre-designed HTML elements, allowing the rapid and simple development of interfaces, for all devices.
-        </p>
+    <div>
+      <div class="homecontainer">
+        <div class="welcome"> 
+          <h1>
+            Simple & <span class="light">light</span><br/> open source <span class="css">CSS</span><br/> framework <span class="ms-text-gray">:)</span>
+          </h1>
+          <p>            
+            minstyle.io integrates a set of pre-designed HTML elements, allowing the rapid and simple development of interfaces, for all devices.
+          </p>
+        </div>
+      </div>
+
+      <div class="actions">
+        
       </div>
     </div>
     );
